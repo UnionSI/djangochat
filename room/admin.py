a17932b1b9fd9bcf2b45151f5a8f41a9f391ff41
gonadr09 <gonzalo_diaz09@hotmail.com>
2023-06-15 15:27:03 -0300

    Start proyect OK

--- room/admin.py
+++ room/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
-from .models import Room
+from .models import Room, Message
 
-admin.site.register(Room)
+@admin.register(Room)
+class AdminRoom(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Message)
+class AdminMessage(admin.ModelAdmin):
+    list_display = ('room', 'user', 'content', 'date_added')
