09be98945487f7bc067ce427c8d3e6f3829bb124
gonadr09 <gonzalo_diaz09@hotmail.com>
2023-11-28 16:15:39 -0300

    Preparado para subir a render

--- djangochat/asgi.py
+++ djangochat/asgi.py
@@ -3,16 +3,16 @@ import django
 
 from django.core.asgi import get_asgi_application
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochat.settings')
+django_asgi_app = get_asgi_application()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-
 import room.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochat.settings')
-#django.setup()
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             room.routing.websocket_urlpatterns
