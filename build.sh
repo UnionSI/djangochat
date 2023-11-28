5f61a02d1849c05d4c8583f2d5e47d2533eda5af
gonadr09 <gonzalo_diaz09@hotmail.com>
2023-11-28 16:43:36 -0300

    Preparado para subir a render

--- build.sh
+++ build.sh
@@ -6,7 +6,13 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
-python manage.py shell <<EOF
-from django.contrib.auth.models import User
-User.objects.create_superuser('admin', 'admin@example.com', '1234')
-EOF
+
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input
+fi
+
+#python manage.py shell <<EOF
+#from django.contrib.auth.models import User
+#User.objects.create_superuser('admin', 'admin@example.com', '1234')
+#EOF
