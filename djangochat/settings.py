56abf20b664695b3260ee69090cd63ced82515ec
gonadr09 <gonzalo_diaz09@hotmail.com>
2023-12-05 16:05:54 -0300

    subir a render v2

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.staticfiles.middleware.StaticFilesMiddleware'
 ]
 
 ROOT_URLCONF = 'djangochat.urls'
