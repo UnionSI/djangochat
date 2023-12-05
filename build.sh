515fde3c7fbb73da20bd047ced22b1f4e8e217ad
gonadr09 <gonzalo_diaz09@hotmail.com>
2023-12-05 00:15:39 -0300

    merge con master-render y ws-globalConsumer

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 #rm .whitenoise_manifest.json
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+python manage.py collectstatic --clear --no-input
 python manage.py makemigrations
 python manage.py makemigrations room
 python manage.py migrate room
