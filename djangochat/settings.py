e97c3471650ea2b6a476c5cfa6a424b6846d588f
gonadr09 <gonzalo_diaz09@hotmail.com>
2023-12-05 01:02:56 -0300

    merge con master-render y ws-globalConsumer

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -153,9 +153,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # si no funciona cambiar a 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # Para whitenoise
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-#WHITENOISE_USE_FINDERS = True
-#WHITENOISE_MANIFEST_STRICT = False
-#WHITENOISE_ALLOW_ALL_ORIGINS = True
+WHITENOISE_USE_FINDERS = True
+WHITENOISE_MANIFEST_STRICT = False
+WHITENOISE_ALLOW_ALL_ORIGINS = True
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
