142c9a0d4ef6ecced842baab544574083c3fc516
gonadr09 <gonzalo_diaz09@hotmail.com>
2023-12-05 15:58:54 -0300

    subir a render v2

--- djangochat/urls.py
+++ djangochat/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', include('core.urls')),
     path('rooms/', include('room.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
