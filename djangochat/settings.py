cfc18a88e169560bfa6a64c43b6a683785c69f44
gonadr09 <gonzalo_diaz09@hotmail.com>
2023-12-05 16:08:16 -0300

    subir a render v2

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -63,7 +63,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.staticfiles.middleware.StaticFilesMiddleware'
 ]
 
 ROOT_URLCONF = 'djangochat.urls'
@@ -153,8 +152,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # si no funciona cambiar a static_root
 
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # Para whitenoise
-WHITENOISE_MAX_AGE = 60
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # Para whitenoise
+#WHITENOISE_MAX_AGE = 60
 #WHITENOISE_USE_FINDERS = True
 #WHITENOISE_MANIFEST_STRICT = False
 #WHITENOISE_ALLOW_ALL_ORIGINS = True
