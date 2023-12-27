22950a9d914dbaf42b72f72c4cf82eabea31cbce
gonadr09 <gonzalo_diaz09@hotmail.com>
2023-12-27 17:21:46 -0300

    configuracion waapi

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ set -o errexit
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --clear --no-input --traceback
-#python manage.py makemigrations
-#python manage.py makemigrations chat
-#python manage.py migrate chat
+python manage.py makemigrations
+python manage.py makemigrations chat
+python manage.py migrate chat
 if [[ $CREATE_SUPERUSER ]];
 then
   python manage.py createsuperuser --no-input
