38d32ae241e0c41329e527b1bdcc66120112bf44
gonadr09 <gonzalo_diaz09@hotmail.com>
2024-02-03 13:10:30 -0300

    estilo final de adjuntos

--- static/js/chatFuncionalidades.js
+++ static/js/chatFuncionalidades.js
@@ -1,10 +1,52 @@
-/* const botonAdjunto = document.querySelector('#file-attached-button')
-const inputAdjuntos = document.getElementById('file-attached')
-botonAdjunto.addEventListener('click', inputAdjuntos.click()) */
+const botonAdjuntar = document.querySelector('#attached-button')
+const botonAgregarAdjunto = document.querySelector('#file-attached-button')
+const botonBorrarAdjunto = document.querySelector('#delete-attached-button')
+const adjuntos = document.querySelector('#file-attached')
+const cantidadAdjuntos = document.querySelector('#amount-files')
+const toastAdjuntos = document.querySelector('#toast-attached')
+const enviarMensaje = document.querySelector('#chat-message-submit')
+
+/* Toast Adjuntos */
+const toastLiveExample = document.getElementById('liveToast')
+const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+
+// Abre la ventana de adjuntos
+botonAgregarAdjunto.addEventListener('click', () => {
+  adjuntos.click()
+})
+
+// Borra los archivos adjuntos
+botonBorrarAdjunto.addEventListener('click', () => {
+  adjuntos.value = '';
+  cantidadAdjuntos.innerHTML = 'Sin archivos adjuntos'
+  toastAdjuntos.innerHTML = 'Se eliminaron todos los adjuntos'
+  toastBootstrap.show()
+  botonAdjuntar.classList.add('btn-light')
+  botonAdjuntar.classList.remove('btn-dark')
+})
+
+// Borra los archivos adjuntos al enviar mensaje
+enviarMensaje.addEventListener('click', () => {
+  adjuntos.value = '';
+  cantidadAdjuntos.innerHTML = 'Sin archivos adjuntos'
+  toastAdjuntos.innerHTML =  'Se eliminaron todos los adjuntos'
+  toastBootstrap.show()
+})
+
+// Informa la cantidad de adjuntos
+adjuntos.addEventListener('change', () => {
+    const numArchivos = adjuntos.files.length;
+    console.log(numArchivos)
+    cantidadAdjuntos.innerHTML = `Archivos adjuntos: ${numArchivos}`
+    toastAdjuntos.innerHTML = `Archivos adjuntos: ${numArchivos}`
+    toastBootstrap.show()
+    botonAdjuntar.classList.remove('btn-light')
+    botonAdjuntar.classList.add('btn-secondary')
+  })
+
+
 /* 
 const emojiButton = document.querySelector('#emoji-button');
-
-
  */
 
 const emojiButton = document.getElementById('emojiButton')
