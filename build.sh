3b0ac4c6f93db9569426f8181e3b438f55392129
gonadr09 <gonzalo_diaz09@hotmail.com>
2024-03-07 10:16:11 -0300

    build.sh

--- build.sh
+++ build.sh
@@ -7,13 +7,11 @@ python -m pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --clear --no-input --traceback
 python manage.py makemigrations
-python manage.py makemigrations usuario
 python manage.py makemigrations chat
-python manage.py makemigrations bot
+python manage.py makemigrations usuario
 python manage.py migrate
-python manage.py migrate usuario
 python manage.py migrate chat
-python manage.py migrate bot
+python manage.py migrate usuario
 #if [[ $CREATE_SUPERUSER ]];
 #then
 #  python manage.py createsuperuser --no-input
